ff81395756ad5ef436b4f27145b4d79c8469fb6b
pablomoreno <pmoretepi@gmail.com>
2015-05-20 11:42:42 -0500

    Agregado de proyecto completo

---
+++ src/TestHelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Created by pablomoreno on 20/05/15.
+ */
+public class TestHelloWorld {
+
+    public static void main(String [] args){
+
+    }
+}
+
+class HelloWord{
+    public void greet(){
+        System.out.println("Hello Word Java");
+    }
+
+}
