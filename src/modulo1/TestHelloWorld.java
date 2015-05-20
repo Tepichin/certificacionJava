ca418a1e5f4529e025ea390b5b0031abb37837db
pablomoreno <pmoretepi@gmail.com>
2015-05-20 13:57:12 -0500

    Agregado de proyecto completo

--- src/modulo1/TestHelloWorld.java
+++ src/modulo1/TestHelloWorld.java
@@ -1,3 +1,5 @@
+package modulo1;
+
 /**
  * Created by pablomoreno on 20/05/15.
  */
---
+++ src/modulo2/TestEmployee.java
@@ -0,0 +1,28 @@
+package modulo2;/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Humberto
+ */
+public class TestEmployee {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Employee e3 = new Employee();
+        Employee e7 = new Employee();
+
+        /*modulo2.Employee e1 = new modulo2.Employee("Jennifer", "Whalen", "JWHALEN", "515.123.4444", "2003/9/17", "Administration Manager", "Administration");
+        modulo2.Employee e2 = new modulo2.Employee("Karen", "Colmenares", "KCOLMENA", "515.127.4566", "2007/8/10)", "Administration Assistant", "Administration");
+        modulo2.Employee e3 = new modulo2.Employee("Michael", "Hartstein", "MHARTSTE", "515.123.5555", "2004, 2, 17", "Marketing Manager", "Marketing");
+        modulo2.Employee e4 = new modulo2.Employee("Pat", "Fay", "PFAY", "603.123.6666", "2005/8/17", "Marketing Representative", "Marketing");
+        modulo2.Employee e5 = new modulo2.Employee("Den", "Raphaely", "DRAPHEAL", "515.127.4561", "2002/7/12", "Purchasing Manager", "Purchasing");
+        modulo2.Employee e6 = new modulo2.Employee("Sigal", "Tobias", "STOBIAS", "515.127.4564", "2005/7/24", "Purchasing Clerk", "Purchasing");
+        modulo2.Employee e7 = new modulo2.Employee("Susan", "Mavris", "SMAVRIS", "515.123.7777", "2002/6/2007", "Human Resources Representative", "HumanResources");
+        modulo2.Employee e8 = new modulo2.Employee("Winston", "Taylor", "WTAYLOR", "650.507.9876", "2006/1/6", "Shipping Clerk", "Shipping");
+        modulo2.Employee e9 = new modulo2.Employee("Alexander", "Hunold", "AHUNOLD", "590.423.4567", "2006/1/3", "Programmer", "IT");
+        */
+    }
+}
