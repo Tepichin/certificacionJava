cc988e8bc42db840e353de0b081f5f918df7444c
pablomoreno <pmoretepi@gmail.com>
2015-05-23 08:36:23 -0500

    Se incluye package modulo3.references y se quita comentarios

--- src/modulo2/test/TestEmployee.java
+++ src/modulo2/test/TestEmployee.java
@@ -31,7 +31,7 @@ public class TestEmployee {
         System.out.println(e7.getFirstName()+", Age: "+e7.getAge());
     */
 
-        Employee e1 = new Employee("Jennifer", "Whalen", "JWHALEN", "515.123.4444", "2003/9/17", "Administration Manager", "Administration");
+/*        Employee e1 = new Employee("Jennifer", "Whalen", "JWHALEN", "515.123.4444", "2003/9/17", "Administration Manager", "Administration");
         Employee e2 = new Employee("Karen", "Colmenares", "KCOLMENA", "515.127.4566", "2007/8/10)", "Administration Assistant", "Administration");
         Employee e3 = new Employee("Michael", "Hartstein", "MHARTSTE", "515.123.5555", "2004, 2, 17", "Marketing Manager", "Marketing");
         Employee e4 = new Employee("Pat", "Fay", "PFAY", "603.123.6666", "2005/8/17", "Marketing Representative", "Marketing");
@@ -39,6 +39,6 @@ public class TestEmployee {
         Employee e6 = new Employee("Sigal", "Tobias", "STOBIAS", "515.127.4564", "2005/7/24", "Purchasing Clerk", "Purchasing");
         Employee e7 = new Employee("Susan", "Mavris", "SMAVRIS", "515.123.7777", "2002/6/2007", "Human Resources Representative", "HumanResources");
         Employee e8 = new Employee("Winston", "Taylor", "WTAYLOR", "650.507.9876", "2006/1/6", "Shipping Clerk", "Shipping");
-        Employee e9 = new Employee("Alexander", "Hunold", "AHUNOLD", "590.423.4567", "2006/1/3", "Programmer", "IT");
+        Employee e9 = new Employee("Alexander", "Hunold", "AHUNOLD", "590.423.4567", "2006/1/3", "Programmer", "IT");*/
     }
 }
